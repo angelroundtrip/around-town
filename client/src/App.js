95d404c308a57555524ad9d3649bb543eeaecfda
Angel Rosario <arosario.us@gmail.com>
2022-04-04 14:49:27 -0400

    React setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
 import { useState, useEffect } from "react";
-import { Routes } from "react-router-dom";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,18 +33,9 @@ function App() {
   }, []);
 
   return (
-
     <div className="App">
-
-   
-    <Routes>
-    
-        {/* <Route path="/url" element={<Component prop={prop} />}/> */}
-
-    </Routes>
-    
-  </div>
-    
+      <h1>Page Count: {count}</h1>
+    </div>
   );
 }
 
