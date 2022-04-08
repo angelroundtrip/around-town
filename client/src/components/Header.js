df4893bee645dffa5af3bf8c0871c72e19053f75
Angel Rosario <arosario.us@gmail.com>
2022-04-08 11:18:09 -0400

    Weather is set dynamically based on user data

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -8,15 +8,21 @@ import React, { useState, useEffect } from 'react'
 
 // console.log(process.env.REACT_APP_WEATHER_API_KEY)
 // * Moving on for now, but come back and hide api key...
-const weatherAPIUrl = 'http://api.weatherapi.com/v1/current.json?key=&q=New York, NY&aqi=no'
+// * API works, refresh changes to null
 
 function Header({user, setUser, handleDeleteAccount}) {
+  
+  
+  const userLocation = user ? user.home_location : null
+  
+  const weatherAPIUrl = `http://api.weatherapi.com/v1/current.json?key=&q=${userLocation}&aqi=no`
+
 
   const [weatherData, setWeatherData] = useState({});
   
   useEffect(() => {
     getWeatherWithFetch();
-  }, []);
+  }, [userLocation]);
 
   const getWeatherWithFetch = async () => {
     const response = await fetch(weatherAPIUrl);
@@ -28,8 +34,6 @@ function Header({user, setUser, handleDeleteAccount}) {
   // const userLocation = user.home_location
   // const WEATHER_API_KEY = ENV['WEATHER_API_KEY']
   // const userLocation = 'New York, NY'
-
-  // * API works when clicked, need to set userLocation dynamically
 
   const handleLogout = () => {
     fetch('/logout',{
@@ -52,7 +56,7 @@ function Header({user, setUser, handleDeleteAccount}) {
       <h1>Header</h1>
 
        {/* Other useful data is available, would like to include weather icon eventually */}
-      <h5> {user ? user.home_location : null} - Temperature: {weatherData.current && weatherData.current.temp_f}°F {weatherData.condition && weatherData.condition.icon} {weatherData.current && weatherData.current.condition && weatherData.current.condition.text}</h5>
+      <h5> {user ? user.home_location : null} -Weather: {weatherData.current && weatherData.current.temp_f}°F {weatherData.condition && weatherData.condition.icon} {weatherData.current && weatherData.current.condition && weatherData.current.condition.text}</h5>
 
       <h2>{user ? user.username : <a href='/' style={{textDecoration: 'none'}}> Login </a>}</h2>
       <h3>{user ? user.home_location : null}</h3>
