d55b048820da45f2e7d597a6a5dfd34baa86f452
Angel Rosario <arosario.us@gmail.com>
2022-04-14 16:06:25 -0400

    Cleaned up code on PostList component

--- client/src/components/PostList.js
+++ client/src/components/PostList.js
@@ -3,14 +3,13 @@ import PostCard from './PostCard'
 
 function PostList({posts, user, handleDeletePosts, weatherData, location}) {
 
-   const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([])
   // * Renders all users
   useEffect(()=>{
     fetch(`/users`)
     .then(r=>r.json())
     .then(setUsers)
   }, [])
-
   // console.log(users)
 
   const [locations, setLocations] = useState([])
@@ -20,12 +19,22 @@ function PostList({posts, user, handleDeletePosts, weatherData, location}) {
     .then(r=>r.json())
     .then(setLocations)
   }, [])
-
   // console.log(locations)
 
   return (
     <div>
-      {posts.map(postObj => <PostCard key={postObj.id} post={postObj} user={user} location={location} handleDeletePosts={handleDeletePosts} weatherData={weatherData} users={users} locations={locations}/>)}
+      {posts.map(postObj => 
+        <PostCard 
+          key={postObj.id} 
+          post={postObj} 
+          user={user} 
+          location={location} 
+          handleDeletePosts={handleDeletePosts} 
+          weatherData={weatherData} 
+          users={users} 
+          locations={locations}
+        />
+      )}
     </div>
   )
 }
